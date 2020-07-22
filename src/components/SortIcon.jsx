a73207920d143cc7f51c042b948b8bc964026b7f
John Pegioudis <thebanforfun@gmail.com>
2020-07-23 00:56:13 +0300

    Simplified SortIcon contents

--- src/components/SortIcon.jsx
+++ src/components/SortIcon.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import styles from "../index.scss";
 
-const SortIcon = () => {
+function SvgComponent(props) {
     return (
-        <svg className={styles.sortIcon}
-            version="1.1" viewBox="0 0 36 36" preserveAspectRatio="xMidYMid meet"
-            xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-            <path d="M29.52,22.52,18,10.6,6.48,22.52a1.7,1.7,0,0,0,2.45,2.36L18,15.49l9.08,9.39a1.7,1.7,0,0,0,2.45-2.36Z" />
+        <svg viewBox="0 0 36 36" className={styles.sortIcon} {...props}>
+            <path d="M29.52 22.52L18 10.6 6.48 22.52a1.7 1.7 0 002.45 2.36L18 15.49l9.08 9.39a1.7 1.7 0 002.45-2.36z"/>
         </svg>
-    );
+    )
 }
 
-export default SortIcon;
+export default SvgComponent

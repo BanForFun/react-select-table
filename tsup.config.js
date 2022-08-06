d4380f8c3de3b1a33a0d7db690e443fcb10f8a33
John Pegioudis <john@petsdb.gr>
2022-08-07 00:23:52 +0300

    Replaced microbundle-crl with tsup

---
+++ tsup.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+import { sassPlugin } from 'esbuild-sass-plugin'
+
+export default defineConfig({
+  entry: ['src/index.js'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  format: ['esm', 'cjs'],
+  onSuccess: 'tsc',
+  esbuildPlugins: [
+    sassPlugin()
+  ]
+})

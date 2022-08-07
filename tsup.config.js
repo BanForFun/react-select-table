66b4d32b3e9fe0ef2b609e662592b30db2fc2858
John Pegioudis <john@petsdb.gr>
2022-08-07 17:06:08 +0300

    Added autoprefixer and postcss-preset-env

--- tsup.config.js
+++ tsup.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'tsup'
 import { sassPlugin } from 'esbuild-sass-plugin'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
+import postcssPresetEnv from 'postcss-preset-env'
+
+const postCss = postcss([
+  autoprefixer,
+  postcssPresetEnv({ stage: 2 })
+])
 
 export default defineConfig({
   entry: ['src/index.js'],
@@ -9,6 +17,11 @@ export default defineConfig({
   format: ['esm', 'cjs'],
   onSuccess: 'run-s build-definitions',
   esbuildPlugins: [
-    sassPlugin()
+    sassPlugin({
+      async transform(source) {
+        const { css } = await postCss.process(source, { from: undefined })
+        return css
+      }
+    })
   ]
 })
